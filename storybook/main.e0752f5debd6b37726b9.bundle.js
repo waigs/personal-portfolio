d1e7bacf9bda357567ce31710226228a47c40c78
Waigwa Gatamu <waigwa.gatamu@berrytech.io>
2019-04-07 09:57:10 +0100

    delete storybook

--- storybook/main.e0752f5debd6b37726b9.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(415),p(491),n.exports=p(888)},491:function(n,o,p){"use strict";p.r(o);p(492),p(856),p(860)}},[[0,1,2]]]);
